a8022e23ebd4d92002a900529ae9144ef04058fb
flip <jr64.naderloo@gmail.com>
2025-06-04 22:36:29 +0330

    change to django

--- src/main/java/AAA/dao/AuserDao.java
+++
@@ -1,8 +0,0 @@
-package AAA.dao;
-
-import AAA.model.Auser;
-import common.GenericDaoImpl;
-
-public class AuserDao extends GenericDaoImpl<Auser, Integer>{
-
-}
--- src/main/java/AAA/dao/RoleDao.java
+++
@@ -1,8 +0,0 @@
-package AAA.dao;
-
-import AAA.model.Role;
-import common.GenericDaoImpl;
-
-public class RoleDao extends GenericDaoImpl<Role, Integer>{
-
-}
